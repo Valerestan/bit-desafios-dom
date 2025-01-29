aca48af868db846b83db0a73ab2457b3be45d241
Vale Restan <valeresmune10@gmail.com>
2025-01-29 17:18:56 -0500

    prueba

--- javascript.js/script1.js
+++ javascript.js/script1.js
@@ -1,2 +1,10 @@
+"use strict";
+
 /* 1. HTML con únicamente un div, replicar la muestra de la manipulación 
 del DOM explicada en clase, Debe mostrar su nombre completo con el texto a color. */
+
+const $nombre = document.getElementById("nombre");
+console.log($nombre);
+
+$nombre.textContent = "Valentina Restan Munera";
+$nombre.className = "color-nombre";
--- javascript.js/script2.js
+++ javascript.js/script2.js
@@ -1,3 +1,16 @@
-/*2. Crear un formulario para que el usuario ingrese: usuario y contraseña, al
-hacer click al botón de enviar mostrarle al usuario una ventana emergente
-informando que se esta procesando la solicitud, imprimir por consola los datos. */
+/*2. Crear un formulario para que el usuario ingrese: usuario y contraseña, 
+al hacer click al botón de enviar mostrarle al usuario una ventana emergente
+informando que se esta procesando la solicitud, almacenar los datos ingresados
+en variables, imprimir por consola los valores de las variables. */
+
+const $dato1 = document.getElementById("usuario");
+const $dato2 = document.getElementById("contrasena");
+const $enviar = document.getElementById("boton");
+
+$enviar.addEventListener("click", enviado);
+
+function enviado() {
+  alert("Se esta procesando la solicitud");
+  console.log($dato1.value);
+  console.log($dato2.value);
+}
